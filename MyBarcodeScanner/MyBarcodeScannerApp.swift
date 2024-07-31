583e2fa9d9f1c9c4d8dfece5d0bfa428e73d6392
Christian Manzaraz <magnusmanz@gmail.com>
2024-07-31 19:20:58 -0300

    Initial Commit

---
+++ MyBarcodeScanner/MyBarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBarcodeScannerApp.swift
+//  MyBarcodeScanner
+//
+//  Created by Christian Manzaraz on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyBarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
