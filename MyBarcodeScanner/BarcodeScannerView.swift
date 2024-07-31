8ba538c6c4404ba1756c12236b91793423203776
Christian Manzaraz <magnusmanz@gmail.com>
2024-07-31 20:40:30 -0300

    Intro & UI Setup

---
+++ MyBarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,38 @@
+//
+//  BarcodeScannerView.swift
+//  MyBarcodeScanner
+//
+//  Created by Christian Manzaraz on 31/07/2024.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    
+    var body: some View {
+        NavigationView {
+            VStack () {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer()
+                    .frame(height: 60)
+                
+                Label("Scanned Barcode",
+                      systemImage: "barcode.viewfinder")
+                .font(.title)
+                
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundStyle(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
